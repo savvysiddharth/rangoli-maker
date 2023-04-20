7a4658d031725e4fc5313307394bc23016311891
Siddharth Maurya <savvysiddharth@gmail.com>
2023-04-20 17:37:31 +0530

    add stroke width slider and remove download button

--- rangoli.js
+++ rangoli.js
@@ -1,16 +1,16 @@
-const downloadbtn = document.querySelector("#download");
+// const downloadbtn = document.querySelector("#download");
 const clearbtn = document.querySelector("#clear");
 const colorpicker = document.querySelector("#colpick");
 
 let r,g,b;
 
 function hideButtons() {
-  downloadbtn.style.opacity = "0";
+  // downloadbtn.style.opacity = "0";
   clearbtn.style.opacity = "0";
 }
 
 function showButtons() {
-  downloadbtn.style.opacity = "1";
+  // downloadbtn.style.opacity = "1";
   clearbtn.style.opacity = "1";
 }
 
@@ -19,10 +19,11 @@ function setup() {
   c.style('margin','0 auto');
   background(0);
   noStroke();
-  //Initializng color
-  r = 102;
-  g = 78;
-  b = 174;
+  // Start with a fixed color
+  // r = 102;
+  // g = 78;
+  // b = 174;
+  doRandomColors();
   translate(width/2,height/2);
   displayInstructions();
 }
@@ -39,18 +40,6 @@ function displayInstructions() {
   text(chars[1], x+50, -220, 200, 100);
   fill(random(0,255),random(0,255),random(0,255));
   text(chars[2], x+120, -220, 200, 100);
-  // for(let i=0; i<7; i++) {
-  //   fill(random(0,255),random(0,255),random(0,255));
-  //   if(random() < 0.5) chars[i] = chars[i].toLowerCase();
-  //   else chars[i] = chars[i].toUpperCase();
-  //   if(i == 6) {
-  //     if(chars[5] == chars[5].toLowerCase()) {
-  //       chars[6] = chars[6].toLowerCase();
-  //     }
-  //   }
-  //   text(chars[i], x, -220, 200, 100);
-  //   x += 50;
-  // }
   textStyle(NORMAL);
   fill(255);
   textSize(32);
@@ -67,6 +56,10 @@ let untouched = true;
 
 function isMouseInCanvas() {
   return mouseX >= 0 && mouseX <= width && mouseY >= 0 && mouseY <= height;
+}
+
+function setDiameterSize(size) {
+  diameter = parseInt(size);
 }
 
 function draw() {
@@ -109,8 +102,8 @@ function randomizeColor() {
 function clearCanvas() {
   window.navigator.vibrate(200);
   background(0);
-  // displayInstructions();
-  // hideButtons();
+  displayInstructions();
+  hideButtons();
   untouched = true;
 }
 
